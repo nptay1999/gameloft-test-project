7dd282cda3fe42883eec8b4bb74e98ff052b8a39
Phuong Tay <taynguyen@digievent.vn>
2023-03-23 14:27:09 +0700

    Feat: Filter community section

--- src/components/sections/Community.tsx
+++ src/components/sections/Community.tsx
@@ -10,89 +10,125 @@ import StorySlick from '../slick/StorySlick';
 import PostSlick from '../slick/PostSlick';
 import PostList from '../list/PostList';
 
+interface ICommunity {
+  id: number;
+  content: string;
+  type: 'facebook' | 'twitter' | 'instagram';
+  link: string;
+  img: string;
+}
+
+const list = [
+  {
+    id: 1,
+    content:
+      'Lorem facebook ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 2,
+    content:
+      'Lorem facebook ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 3,
+    content:
+      'Lorem facebook ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 4,
+    content:
+      'Lorem twitter ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'twitter' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 5,
+    content:
+      'Lorem twitter ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'twitter' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 6,
+    content:
+      'Lorem twitter ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'twitter' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 7,
+    content:
+      'Lorem instagram ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'instagram' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 8,
+    content:
+      'Lorem instagram ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'instagram' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 9,
+    content:
+      'Lorem instagram ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'instagram' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+  {
+    id: 10,
+    content:
+      'Lorem instagram ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
+    type: 'instagram' as 'facebook' | 'twitter' | 'instagram',
+    link: '/#',
+    img: CardBg,
+  },
+];
+
 function Community(): JSX.Element {
-  const list = [
-    {
-      id: 1,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 2,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 3,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 4,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 5,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 6,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 7,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 8,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 9,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-    {
-      id: 10,
-      content:
-        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos.',
-      type: 'facebook' as 'facebook' | 'twitter' | 'instagram',
-      link: '/#',
-      img: CardBg,
-    },
-  ];
+  const searchRef = React.useRef<any>(null);
+  const [showList, setShowList] = React.useState<ICommunity[]>(list);
+  const filterList = (filter: string): void => {
+    if (filter === 'facebook') {
+      const listFilter = list.filter((item) => item.type === 'facebook');
+      setShowList(listFilter);
+    } else if (filter === 'twitter') {
+      const listFilter = list.filter((item) => item.type === 'twitter');
+      setShowList(listFilter);
+    } else if (filter === 'instagram') {
+      const listFilter = list.filter((item) => item.type === 'instagram');
+      setShowList(listFilter);
+    } else {
+      setShowList(list);
+    }
+  };
+  const filterAction = (keyword: string): void => {
+    const listFilter = list.filter((item) => item.content.includes(keyword));
+    setShowList(listFilter);
+  };
+  const enterSearch = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') {
+      const keyword = searchRef.current.value;
+      filterAction(keyword);
+      searchRef.current.value = '';
+    }
+  };
   return (
     <article className="community">
       <div className="community__container">
@@ -105,20 +141,46 @@ function Community(): JSX.Element {
         </section>
         <section className="community__post">
           <div className="community__post--head">
-            <h3 className="community__post--title">All Posts</h3>
+            <h3
+              className="community__post--title"
+              onClick={() => filterList('')}
+              aria-hidden="true"
+            >
+              All Posts
+            </h3>
             <div className="community__post--social">
-              <img src={Twitter} alt="twitter" />
-              <img src={Instagram} alt="instagram" />
-              <img src={Facebook} alt="facebook" />
+              <img
+                src={Twitter}
+                alt="twitter"
+                onClick={() => filterList('twitter')}
+                aria-hidden="true"
+              />
+              <img
+                src={Instagram}
+                alt="instagram"
+                onClick={() => filterList('instagram')}
+                aria-hidden="true"
+              />
+              <img
+                src={Facebook}
+                alt="facebook"
+                onClick={() => filterList('facebook')}
+                aria-hidden="true"
+              />
             </div>
             <div className="community__post--search">
-              <input type="text" placeholder="search" />
+              <input
+                type="text"
+                placeholder="search"
+                ref={searchRef}
+                onKeyUp={enterSearch}
+              />
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </div>
           </div>
           <div className="community__post--content">
-            <PostSlick list={list} />
-            <PostList list={list} />
+            <PostSlick list={showList} />
+            <PostList list={showList} />
           </div>
         </section>
       </div>
